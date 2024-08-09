351ac148d5f4589c852f4ec663e0ffb1b5b9e942
Robert Dyro <rdyro@google.com>
2024-08-09 14:22:35 -0700

    adding permissive pytree serialization methods

--- jax/experimental/array_serialization/new_api.py
+++ jax/experimental/array_serialization/new_api.py
@@ -21,9 +21,12 @@ import json
 import asyncio
 import threading
 import shutil
+import pickle
+import logging
+from dataclasses import dataclass
 
 import jax
-from jax.tree_util import PyTreeDef, default_registry
+from jax.tree_util import PyTreeDef, default_registry, treedef_is_leaf
 from jax import tree
 from jax.experimental.array_serialization import serialization
 from jax.experimental.array_serialization.serialization import get_tensorstore_spec
@@ -84,23 +87,11 @@ def _is_remote_path(path: str | PathLike):
   return any(str(path).startswith(prefix[:-1]) for prefix in 
              _REMOTE_URL_PREFIXES)
 
-def serialize_pytreedef(tree: PyTreeDef) -> bytes:
-  # check that the tree deserializes with the default registry
-  try:
-    pytreedef_data = PyTreeDef.serialize_using_proto(tree)
-  except TypeError:
-    raise NotImplementedError("Attempting to serialize a PyTreeDef that does"
-                              " has custom types. This is not currently"
-                              " supported.")
-  return pytreedef_data
-
-def deserialize_pytreedef(tree_bytes: bytes, 
-                          registry = default_registry) -> PyTreeDef:
-  return PyTreeDef.deserialize_using_proto(registry, tree_bytes)
-  
-
 async def async_save(data: Any, directory: str | PathLike, 
-                     overwrite: bool = True) -> None:
+                     overwrite: bool = True, custom_types: bool = False) -> None:
+  if custom_types:
+    raise NotImplementedError("We don't currently support saving pytrees with"
+                              " custom types.")
   assert not _is_remote_path(directory) or epath_installed, (
     "For checkpointing using remote URLs (e.g., gs, s3) you need `etils`"
     " module installed. You can install it using `pip install etils`."
@@ -128,20 +119,17 @@ async def async_save(data: Any, directory: str | PathLike,
   paths = [root / f"{field_name}{_get_suffix(data)}"
                    for field_name, data in zip(field_names, data_flat)]
 
-  # serialize non-array objects, avoid the zip(*[]) for clarity 
-  obj_flat = [data for path, data in zip(paths, data_flat) 
-              if path.suffix != _TENSORSTORE_SUFFIX]
-  obj_paths = [path for path in paths if path.suffix != _TENSORSTORE_SUFFIX]
-
+  obj_flat, obj_paths = zip(*[(data, path) 
+                              for path, data in zip(paths, data_flat) 
+                              if path.suffix != _TENSORSTORE_SUFFIX])
   serialize_futures = []
 
   for data, path in zip(obj_flat, obj_paths):
     serialize_futures.append(asyncio.to_thread(_obj_serialize, data, path))
 
-  # serialize arrays, avoid the zip(*[]) for clarity 
-  arr_flat = [data for path, data in zip(paths, data_flat) 
-              if path.suffix == _TENSORSTORE_SUFFIX] 
-  arr_paths = [path for path in paths if path.suffix == _TENSORSTORE_SUFFIX]
+  arr_flat, arr_paths = zip(*[(data, path) for path, data 
+                              in zip(paths, data_flat) 
+                              if path.suffix == _TENSORSTORE_SUFFIX])
   ts_specs = [get_tensorstore_spec(path) for path in arr_paths]
   serialize_futures.extend([serialization.async_serialize(arr, ts_spec) 
                             for (arr, ts_spec) in zip(arr_flat, ts_specs)])
@@ -181,18 +169,17 @@ async def async_load(directory: str | PathLike,
   arrs = {path.stem: serialization.async_deserialize(sharding, ts_spec) 
               for (sharding, ts_spec, path) 
               in zip(shardings, ts_specs, arr_paths)}
-  arr_objs = objs | arrs
+  arr_objs = objs | arrs # pythonic join dictionaries 
   filled_values = await asyncio.gather(*[arr_objs[value] for value in values])
   return tree.unflatten(pytreedef, filled_values)
 
   
-def load_pytree(data: Any, directory: str | PathLike) -> None:
-  def _async_save():
-    asyncio.run(async_save(data=data, directory=directory))
-
-  thread = threading.Thread(target=_async_save)
-  thread.start()
-  thread.join()
+def load_pytree(directory: str | PathLike) -> None:
+  assert not _is_remote_path(directory) or epath_installed, (
+    "For checkpointing using remote URLs (e.g., gs, s3) you need `etils`"
+    " module installed. You can install it using `pip install etils`.")
+  root = Path(directory)
+  return json.loads((root / _PYTREEDEF_FILE).read_text())
 
 def save(data: Any, directory: str | PathLike) -> None:
   def _async_save():
@@ -212,3 +199,112 @@ def load(directory: str | PathLike, shardings: Sequence | None = None,
   thread.start()
   thread.join()
   return result[0]
+  
+################################################################################
+
+_BUILTINS_MAP = {
+  "builtins.dict": dict,
+  "builtins.list": list,
+  "builtins.tuple": tuple,
+  "builtins.set": set,
+}
+_BUILTINS_SET = set(_BUILTINS_MAP.values())
+
+def _cls2typerepr(cls):
+  return f"{cls.__module__}.{cls.__name__}"
+  
+def _encode_node_data(node_data, node_data_store: dict[str, Any]):
+  if node_data is None:
+    return None
+  elif node_data[0] in _BUILTINS_SET:
+    return (_cls2typerepr(node_data[0]), node_data[1])
+  else:
+    node_data_bytes = pickle.dumps(node_data)
+    node_data_key = str(uuid4())
+    node_data_store[node_data_key] = node_data_bytes
+    return (_cls2typerepr(node_data[0]), node_data_key)
+
+def _decode_node_data(node_data_encoded, node_data_store: dict[str, Any]):
+  if node_data_encoded is None:
+    return None
+  elif node_data_encoded[0] in _BUILTINS_MAP:
+    return (_BUILTINS_MAP[node_data_encoded[0]], node_data_encoded[1])
+  else:
+    node_data_key = node_data_encoded[1]
+    if node_data_key in node_data_store:
+      try:
+        return pickle.loads(node_data_store[node_data_key])
+      except (pickle.UnpicklingError, AttributeError):
+        pass
+    # fallback
+    logging.warning(f"Unrecognized data type {node_data_encoded[0]} we'll do"
+                    " our best and just return a list of children")
+    return (list, None)
+
+def _serialize_pytree_helper(node, node_data_store: dict[str, Any]):
+  node_data_store = node_data_store or dict()
+  node_repr = dict()
+  node_repr["encoded_node_data"] = _encode_node_data(node.node_data(), 
+                                                     node_data_store)
+  # this encodes if-and-only-if using not and xor
+  assert not ((len(node.children()) == 0) ^ treedef_is_leaf(node))
+
+  leaf_ids = None
+  if not treedef_is_leaf(node):
+    children, leaf_ids = zip(*[
+      _serialize_pytree_helper(child, node_data_store=node_data_store) 
+      for child in node.children()])
+    node_repr["children"] = children
+  else:
+    node_repr["children"] = []
+    assert node.num_leaves in (0, 1), (
+      "We only support 1 or 0 leaves (?) are a leaf")
+    node_repr["leaf_id"] = str(uuid4()) if node.num_leaves == 1 else None
+    leaf_ids = node_repr["leaf_id"]
+
+  return node_repr, leaf_ids
+
+class AbstractPyTreeSerialization:
+  @staticmethod
+  def serialize_pytree(node):
+    raise NotImplementedError
+
+  @staticmethod
+  def deserialize_pytree(node: dict[str, Any], 
+                         node_data_store: dict[str, Any] | None = None):
+    raise NotImplementedError
+
+class PermissivePyTreeSerialization(AbstractPyTreeSerialization):
+  @staticmethod
+  def serialize_pytree(node):
+    node_data_store = dict()
+    node_repr, leaf_ids = _serialize_pytree_helper(node, node_data_store)
+    node_repr["permissive"] = True
+    return node_repr, leaf_ids, node_data_store
+
+  @staticmethod
+  def deserialize_pytree(node: dict[str, Any], 
+                        node_data_store: dict[str, Any] | None = None):
+    if node_data_store is None:
+      node_data_store = dict()
+    assert "encoded_node_data" in node and "children" in node
+    pytree_children = [PermissivePyTreeSerialization.deserialize_pytree(child) 
+                       for child in node["children"]]
+    node_data = _decode_node_data(node["encoded_node_data"], node_data_store)
+    pt = PyTreeDef.make_from_node_data_and_children(default_registry, node_data,
+                                                    pytree_children)
+    return pt
+
+class StrictPyTreeSerialization(AbstractPyTreeSerialization):
+  @staticmethod
+  def serialize_pytree(node):
+    leaf_ids = [str(uuid4()) for _ in range(node.num_leaves)]
+    assert _is_pytree_serializable(node)
+    node_repr = tree.unflatten(node, leaf_ids)
+    node_repr["permissive"] = False
+    return node_repr, leaf_ids, dict()
+
+  @staticmethod
+  def deserialize_pytree(node: dict[str, Any], 
+                         node_data_store: dict[str, Any] | None = None):
+    return tree.structure(node)
